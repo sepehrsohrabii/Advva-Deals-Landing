ccf075062d9a5b7b437420f8c1774727a1d79e5d
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2022-06-05 02:03:55 +0430

    Completed before index.php

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     if(wn > 120){
     	$(".navbar").css("background","#f2f2f2");
       $(".navbar").removeClass("mt-md-5");
-      $(".navbar").css("transition","0.5s");
+      $(".navbar").css("transition","0.3s");
     }
     else{
     	$(".navbar").css("background","none");
@@ -55,3 +55,245 @@ $(document).ready(function(){"use strict";
 })(jQuery);
 
 
+/* navbar-new - START */
+const html = document.documentElement;
+const toggle = document.getElementById("toggle");
+const circle = document.getElementById("bg-circle");
+const navlinks = document.getElementById("navlinks")
+const circleWidth = circle.clientWidth;
+
+// Math calcul to get Height, Width, Diagonal and Circle Radius
+
+const getVpdr = () => {
+  const vph = Math.pow(html.offsetHeight, 2); // Height
+  const vpw = Math.pow(html.offsetWidth, 2); // Width
+  const vpd = Math.sqrt(vph + vpw); // Diagonal
+  return vpd * 2 / circleWidth; // Circle radius
+};
+
+const openNavbar = () => {
+  const openTimeline = new TimelineMax();
+  openTimeline.to(".navbar-new", 0, { display: "flex" });
+  openTimeline.to("#bg-circle", 1.5, { scale: getVpdr(), ease: Expo.easeInOut });
+  openTimeline.staggerFromTo(".navbar-new ul li", 0.5, { y: 25, opacity: 0 }, { y: 0, opacity: 1 }, 0.1, 1);
+};
+
+const closeNavbar = () => {
+  const closeTimeline = new TimelineMax();
+  closeTimeline.staggerFromTo(".navbar-new ul li", 0.5, { y: 0, opacity: 1, delay: 0.5 }, { y: 25, opacity: 0 }, -0.1);
+  closeTimeline.to("#bg-circle", 1, { scale: 0, ease: Expo.easeInOut, delay: -0.5 });
+  closeTimeline.to(".navbar-new", 0, { display: "none" });
+};
+
+let isOpen = false;
+
+toggle.onclick = function () {
+  if (isOpen) {
+    this.classList.remove("active");
+    closeNavbar();
+  } else {
+    this.classList.add("active");
+    openNavbar();
+  }
+  isOpen = !isOpen;
+};
+circle.onclick = function () {
+  if (isOpen) {
+    toggle.classList.remove("active");
+    closeNavbar();
+  } else {
+    toggle.classList.add("active");
+    closeNavbar();
+  }
+  isOpen = !isOpen;
+};
+navlinks.onclick = function () {
+  if (isOpen) {
+    toggle.classList.remove("active");
+    closeNavbar();
+  } else {
+    toggle.classList.add("active");
+    closeNavbar();
+  }
+  isOpen = !isOpen;
+}
+
+
+// On windows resize, recalcule circle radius and update
+
+window.onresize = () => {
+  if (isOpen) {
+    gsap.to("#bg-circle", 1, { scale: getVpdr(), ease: Expo.easeInOut });
+  }
+};
+/* Navabr - END */
+
+
+/* LazyLoad - START */
+gsap.registerPlugin(ScrollTrigger);
+  
+gsap.from(".gsap1", {
+  scrollTrigger: {
+    trigger: ".gsap1",
+    start: "top bottom",
+    end: "bottom 0%",
+    toggleActions: "play reset play reset"
+  },
+  x: -500,
+  duration: 1
+});
+gsap.from(".gsap2", {
+  scrollTrigger: {
+    trigger: ".gsap2",
+    start: "top bottom",
+    end: "bottom 0%",
+    toggleActions: "play reset play reset"
+  },
+  x: -500,
+  duration: 1
+});
+gsap.from(".image5", {
+  scrollTrigger: {
+    trigger: ".image5",
+    start: "top bottom",
+    end: "bottom 0%",
+    toggleActions: "play reset play reset"
+  },
+  x: +500,
+  rotation:360,
+  duration: 1
+});
+gsap.from(".image4", {
+  scrollTrigger: {
+    trigger: ".image4",
+    start: "top bottom",
+    end: "bottom 0%",
+    toggleActions: "play reset play reset"
+  },
+  rotation:45,
+  duration: 1
+});
+gsap.from(".image3", {
+  scrollTrigger: {
+    trigger: ".image3",
+    start: "top bottom",
+    end: "bottom 0%",
+    toggleActions: "play reset play reset"
+  },
+  rotation:45,
+  scale: 0.2,
+  duration: 1
+});
+gsap.from(".image9", {
+  scrollTrigger: {
+    trigger: ".image9",
+    start: "top bottom",
+    end: "bottom 20%",
+    toggleActions: "play reset play reset",
+    scrub: 1,
+  },
+  rotation: +45,
+  y:-150,
+  scale: 0.2,
+  duration: 1
+});
+gsap.from(".image10", {
+  scrollTrigger: {
+    trigger: ".image10",
+    start: "top bottom",
+    end: "bottom 20%",
+    toggleActions: "play reset play reset",
+    scrub: 1,
+  },
+  y:-150,
+  scale: 0.2,
+  duration: 1
+});
+gsap.from(".image11", {
+  scrollTrigger: {
+    trigger: ".image11",
+    start: "top bottom",
+    end: "bottom 20%",
+    toggleActions: "play reset play reset",
+    scrub: 1,
+  },
+  rotation: -45,
+  y:-150,
+  scale: 0.2,
+  duration: 1
+});
+gsap.from(".image6", {
+  scrollTrigger: {
+    trigger: ".image6",
+    start: "top bottom",
+    end: "bottom 20%",
+    toggleActions: "play reset play reset",
+    scrub: 1,
+  },
+  rotation: +45,
+  scale: 0.2,
+  duration: 1
+});
+gsap.from(".image7", {
+  scrollTrigger: {
+    trigger: ".image7",
+    start: "top bottom",
+    end: "bottom 50%",
+    toggleActions: "play reset play reset",
+    scrub: 1,
+  },
+  rotation: 360*2,
+  x:-150,
+  scale: 0.6,
+  duration: 1
+});
+gsap.from(".image8", {
+  scrollTrigger: {
+    trigger: ".image8",
+    start: "top bottom",
+    end: "bottom 50%",
+    toggleActions: "play reset play reset",
+    scrub: 1,
+  },
+  rotation: 360*2,
+  x:-150,
+  scale: 0.2,
+  duration: 1
+});
+gsap.from(".image2", {
+  scrollTrigger: {
+    trigger: ".image2",
+    start: "top bottom",
+    end: "bottom 50%",
+    toggleActions: "play reset play reset",
+    scrub: 1,
+  },
+  rotation: +20,
+  duration: 1
+});
+
+gsap.from(".gsap3", {
+  scrollTrigger: {
+    trigger: ".gsap3",
+    start: "top bottom",
+    end: "bottom 20%",
+    toggleActions: "play reset play reset",
+    scrub: 1,
+  },
+  x:+600,
+  duration: 1
+});
+
+
+/* Lazy Load for sections */
+var sections = gsap.utils.toArray('.section');
+sections.forEach((section) => {
+  gsap.from(section, {
+    scrollTrigger: {
+      trigger: section,
+      toggleActions: "play reverse play reverse",
+    },
+    opacity: 0,
+    duration: 1
+  });
+})
